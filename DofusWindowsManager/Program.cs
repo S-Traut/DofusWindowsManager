d546a69e07b390c74bc876d7069db460f335a4e7
Stéphane Traut <stephane.traut@gmail.com>
2020-12-07 23:37:46 +0100

    init

---
+++ DofusWindowsManager/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DofusWindowsManager
+{
+    class Program
+    {
+        private static WindowManager windowsManager;
+        private static bool space = false;
+        private static bool alt = false;
+
+        [STAThread]
+        static void Main()
+        {
+            Console.Title = "Dofus Windows Manager | By PoNo";
+            Console.Write("Dofus Windows Manager - Created by PoNo\nFor more informations check the git page [...]\n");
+            windowsManager = new WindowManager();
+            InputReader reader = new InputReader();
+            reader.OnKeyPressed += OnKeyPressed;
+            reader.OnKeyUnpressed += OnKeyRelease;
+            reader.HookKeyboard();
+            Application.Run();
+            reader.UnHookKeyboard();
+        }
+
+        private static void OnKeyRelease(object sender, Keys e)
+        {
+            if(e == Keys.Space) space = false;
+            if(e == Keys.LMenu) alt = false;
+        }
+
+        private static void OnKeyPressed(object sender, Keys e)
+        {
+            if(e == Keys.Space) space = true;
+            if(e == Keys.LMenu) alt = true;
+            if(e == Keys.Tab && !alt) windowsManager.NextWindow();
+            if(space)
+            {
+                switch(e)
+                {
+                    case Keys.D1: windowsManager.SwitchWindow(0); break;
+                    case Keys.D2: windowsManager.SwitchWindow(1); break;
+                    case Keys.D3: windowsManager.SwitchWindow(2); break;
+                    case Keys.D4: windowsManager.SwitchWindow(3); break;
+                    case Keys.D5: windowsManager.SwitchWindow(4); break;
+                    case Keys.D6: windowsManager.SwitchWindow(5); break;
+                    case Keys.D7: windowsManager.SwitchWindow(6); break;
+                    case Keys.D8: windowsManager.SwitchWindow(7); break;
+                }
+            }
+        }
+    }
+}
---
+++ DofusWindowsManager/WindowsManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Media;
+
+namespace DofusWindowsManager
+{
+    public class WindowManager
+    {
+        private const int ALT = 0xA4;
+        private const int EXTENDEDKEY = 0x1;
+        private const int KEYUP = 0x2;
+        private int currentIndex = 0;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool SetForegroundWindow(IntPtr windowHandle);
+
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        public void SwitchWindow(int index)
+        {  
+            try
+            {
+                var fullProcess = Process.GetProcessesByName("dofus");
+                if (index >= fullProcess.Length)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("WARNING: The window index is not openned\n");
+                    SystemSounds.Beep.Play();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return;
+                }
+                Process process = fullProcess[index];
+                if (process != null)
+                {
+                    keybd_event((byte)ALT, 0x45, EXTENDEDKEY | 0, 0);
+                    SetForegroundWindow(process.MainWindowHandle);
+                    keybd_event((byte)ALT, 0x45, EXTENDEDKEY | KEYUP, 0);
+                    Console.Write($"Windows as been switched to index {index}\n");
+                    currentIndex = index;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: Application is not running!\nException: {ex.Message}\n");
+                Console.Read();
+                return;
+            }
+        }
+
+        public void NextWindow()
+        {
+            int toIndex = currentIndex == Process.GetProcessesByName("dofus").Length - 1 ? 0 : currentIndex + 1;
+            SwitchWindow(toIndex);
+        }
+    }
+}
